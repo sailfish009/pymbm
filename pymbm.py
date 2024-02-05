3f678a71da4930491d15a2d7e300a86102e63f8c
sailfish009 <bluefish9999@protonmail.com>
2024-02-06 06:43:29 +0900

    Add files via upload

--- pymbm.py
+++ pymbm.py
@@ -28,13 +28,13 @@ category = get_category()
 set_side_category(category)
 category_add()
 
-# custom font : Korean Hangul Font
-with dpg.font_registry():
-    # with dpg.font(".\\NotoSansKR-Medium.ttf", 16) as default_font:
-    # with dpg.font(".\\NotoSerifCJKkr-Medium.otf", 16) as default_font:
-    with dpg.font("C:\\Windows\\Fonts\\Malgun.ttf", 20) as default_font:
-        dpg.add_font_range_hint(dpg.mvFontRangeHint_Korean)
-    dpg.bind_font(default_font)
+# # custom font : Korean Hangul Font
+# with dpg.font_registry():
+#     # with dpg.font(".\\NotoSansKR-Medium.ttf", 16) as default_font:
+#     # with dpg.font(".\\NotoSerifCJKkr-Medium.otf", 16) as default_font:
+#     with dpg.font("C:\\Windows\\Fonts\\Malgun.ttf", 20) as default_font:
+#         dpg.add_font_range_hint(dpg.mvFontRangeHint_Korean)
+#     dpg.bind_font(default_font)
 
 ###################################################################################################
 
